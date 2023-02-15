d936265bbe9a6c76dde92cc39e91d9f922391a44
s6anring <102953074+s6anring@users.noreply.github.com>
2023-02-15 17:55:27 +0100

    Transformed the html data to one pandas dataframe

--- prep/parser.py
+++ prep/parser.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from bs4 import BeautifulSoup
+from scraper import months
 
-# select one year and identify
+dfs = []
+for month in months:
+    with open(f"prep/data/{month}.html") as f:
+        page = f.read()
+    soup = BeautifulSoup(page, "html.parser")
+    table = soup.find(id="schedule")
+    table_pd = pd.read_html(str(table), flavor="bs4")[0]
+
+    dfs.append(table_pd)
+
+df = pd.concat(dfs)
 
 
-# for month in months
-with open("prep/data/october.html") as f:
-    page = f.read()
-
-soup = BeautifulSoup(page, "html.parser")
-
-october_table = soup.find(id="schedule")
-
-october_table_pd = pd.read_html(str(october_table))
+df.to_csv("./src/data/data.csv")
+df.to_pickle("./src/data/data.pkl")
