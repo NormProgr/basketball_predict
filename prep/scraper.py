bb4932e7abd431da4e99e5b0a1a1f056b5cfc62f
Norman Metzinger <norman.metzinger@gmx.de>
2023-02-15 15:27:49 +0100

    Build a Scraper and donwloaded the data scraped

---
+++ prep/scraper.py
@@ -0,0 +1,25 @@
+import requests as req
+
+url_start = "https://www.basketball-reference.com/leagues/NBA_2023_games-{}.html"
+# replace october by month string with bracket{}
+
+months = ["october", "november", "december", "january", "february", "march", "april"]
+
+
+def scraper_by_month(months, url_start):
+    """Scrape the data by month and save it.
+
+    Args:
+        months (list): List of months to be scraped.
+        url_start (url): Source url to scrape data.
+
+    """
+    for month in months:
+        url = url_start.format(month)
+        data = req.get(url)
+
+        with open(f"prep/data/{month}.html", "w+") as f:
+            f.write(data.text)
+
+
+scraper_by_month(months, url_start)
