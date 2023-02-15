8e49afe3139e9e694956561bd00e81a922745969
Norman Metzinger <norman.metzinger@gmx.de>
2023-02-15 17:12:03 +0100

    Wrote parts of the parser

--- prep/parser.py
+++ prep/parser.py
@@ -1,13 +1,5 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-
 # select one year and identify
 
-with open("prep/data/april.html") as f:
-    page = f.read()
 
-soup = BeautifulSoup(page, "html.parser")
-
-april_table = soup.find(id="schedule")
-
-april_table_pd = pd.read_html(april_table)
+# for month in months
+# with open("prep/data/october.html") as f:
