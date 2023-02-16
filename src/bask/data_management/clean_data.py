3fdd6c8fc0ac88300b2b06c697d20a2b6183c906
s6anring <102953074+s6anring@users.noreply.github.com>
2023-02-16 14:34:57 +0100

    data cleaning functions

--- src/bask/data_management/clean_data.py
+++ src/bask/data_management/clean_data.py
@@ -4,19 +4,34 @@ import pandas as pd
 import yaml
 
 
-def clean_data():
+def clean_columns():
     """Remember that the name has to be flexible."""
     with open("./src/bask/data_management/data_info.yaml") as file:
         data_info = yaml.safe_load(file)
     df = pd.read_pickle("./src/bask/data/data_2023-02-16.pkl")
     df.drop(columns=data_info["columns_to_drop"], inplace=True)
     df.rename(columns=data_info["column_rename"], inplace=True)
-    df["home_wins"] = np.where(df["pts_home"] > df["pts_visitor"], 1, 0)
-
     return df
 
 
-clean_data()
+def win_col(df):
+    df["home_wins"] = np.where(
+        df[["pts_home", "pts_visitor"]].isna().any(axis=1),
+        np.nan,
+        np.where(df["pts_home"] > df["pts_visitor"], 1, 0),
+    )
+    return df
 
-# change dateformat
-# add winner
+
+def transform_date(df):
+    df["Date"] = pd.to_datetime(df["Date"])
+    return df
+
+
+def clean_data():
+    df = transform_date(clean_columns())
+    df = win_col(df)
+    return df
+
+
+df = clean_data()
--- src/bask/data_management/task_data_management.py
+++ src/bask/data_management/task_data_management.py
@@ -1,1 +1,13 @@
 """Tasks for managing the data."""
+
+from clean_data import *
+
+# combine steps from cleaning
+
+
+def data_management():
+    df = transform_date(clean_data())
+    return df
+
+
+df = data_management()
