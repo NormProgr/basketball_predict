7d9f07f9dbe57222945f38956fd0aa1e48687747
Norman Metzinger <norman.metzinger@gmx.de>
2023-02-16 10:17:24 +0100

    Made code of scraper.py nicer

--- prep/parser.py
+++ prep/parser.py
@@ -2,18 +2,24 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from scraper import months
 
-dfs = []
-for month in months:
-    with open(f"prep/data/{month}.html") as f:
-        page = f.read()
-    soup = BeautifulSoup(page, "html.parser")
-    table = soup.find(id="schedule")
-    table_pd = pd.read_html(str(table), flavor="bs4")[0]
 
-    dfs.append(table_pd)
+def convert_to_df(months=months):
+    dfs = []
+    for month in months:
+        with open(f"prep/data/{month}.html") as f:
+            page = f.read()
+        soup = BeautifulSoup(page, "html.parser")
+        table = soup.find(id="schedule")
+        table_pd = pd.read_html(str(table), flavor="bs4")[0]
+        dfs.append(table_pd)
+    df = pd.concat(dfs)
+    return df
 
-df = pd.concat(dfs)
+
+def produce_data():
+    df = convert_to_df()
+    df.to_csv("./src/data/data.csv")
+    df.to_pickle("./src/data/data.pkl")
 
 
-df.to_csv("./src/data/data.csv")
-df.to_pickle("./src/data/data.pkl")
+produce_data()
