24ef20387c6344ac95c2b18807fc69ae6f9267b8
s6anring <102953074+s6anring@users.noreply.github.com>
2023-02-16 14:37:58 +0100

    data cleaning functions

--- src/bask/data_management/task_data_management.py
+++ src/bask/data_management/task_data_management.py
@@ -1,13 +1,1 @@
 """Tasks for managing the data."""
-
-from clean_data import *
-
-# combine steps from cleaning
-
-
-def data_management():
-    df = transform_date(clean_data())
-    return df
-
-
-df = data_management()
