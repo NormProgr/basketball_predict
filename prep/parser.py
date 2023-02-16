d40c37d769e35c9f5b2511c25fd118732f1ea8b0
s6anring <102953074+s6anring@users.noreply.github.com>
2023-02-16 11:03:06 +0100

    Tested code in mac

--- prep/parser.py
+++ prep/parser.py
@@ -1,9 +1,19 @@
 import pandas as pd
 from bs4 import BeautifulSoup
-from scraper import months
+
+months = ["october", "november", "december", "january", "february", "march", "april"]
 
 
 def convert_to_df(months=months):
+    """Convert tables from html files to one pandas DataFrame.
+
+    Args:
+        months (list): list of months to be added to combined dataframe.
+
+    Returns:
+        df (pandas DataFrame): Concatenated df with all entries from html tables.
+
+    """
     dfs = []
     for month in months:
         with open(f"prep/data/{month}.html") as f:
@@ -17,9 +27,10 @@ def convert_to_df(months=months):
 
 
 def produce_data():
+    """Save pandas df with entries from html tables in src folder."""
     df = convert_to_df()
-    df.to_csv("./src/data/data.csv")
-    df.to_pickle("./src/data/data.pkl")
+    df.to_csv("./src/bask/data/data.csv")
+    df.to_pickle("./src/bask/data/data.pkl")
 
 
 produce_data()
