3e7e8e1c281c9e146c2fb27434b48615de138227
Norman Metzinger <norman.metzinger@gmx.de>
2023-02-27 11:51:25 +0100

    Wrote tests for test_predict and added a new data_fixture

--- tests/analysis/test_evaluation.py
+++ tests/analysis/test_evaluation.py
@@ -1,1 +1,25 @@
 """Tests for the model evaluations."""
+
+import pandas as pd
+import pytest
+from bask.config import TEST_DIR
+
+
+@pytest.fixture()
+def data():
+    return pd.read_csv(TEST_DIR / "analysis" / "data_fixture.csv")
+
+
+@pytest.fixture()
+def data_pred():
+    return pd.read_csv(TEST_DIR / "analysis" / "data_fixture_pred.csv")
+
+
+@pytest.fixture()
+def data_pred():
+    return pd.read_csv(TEST_DIR / "analysis" / "data_fixture_benchmark.csv")
+
+
+@pytest.fixture()
+def data_pred():
+    return pd.read_csv(TEST_DIR / "analysis" / "data_fixture_benchmark_pred.csv")
