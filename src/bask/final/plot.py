80baf7a12575f1a2f80b2415af169010c2536023
Norman Metzinger <norman.metzinger@gmx.de>
2023-02-28 19:59:43 +0100

    Finalized analysis by producing more tests and redirect the output

--- src/bask/final/plot.py
+++ src/bask/final/plot.py
@@ -1,4 +1,10 @@
 """Functions plotting results."""
+import pandas as pd
+
+data = pd.read_csv("bld/python/predictions/prediction_scores.csv")
+data_scores = pd.read_csv("bld/python/predictions/concatenated_pred.csv")
+data_pred = pd.read_csv("bld/python/predictions/result_prediction.csv")
+
 
 # heatmap of how well the model performed
 
